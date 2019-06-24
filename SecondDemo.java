7a23f66b1177785dd3e04a2f1232bed474cde836
kurukundaveera <46889578+kurukundaveera@users.noreply.github.com>
2019-06-24 22:21:53 +0530

    Webhooks Second Commit

---
+++ SecondDemo.java
@@ -0,0 +1,7 @@
+public class SecondDemo {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello Welcome to Devops Webhooks");
+      System.out.println("God Please select us for ING Project as Devops Engineer");
+   }
+}
